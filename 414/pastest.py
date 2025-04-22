bcfcbf2d069d3a9b4111d92d81071cb8ef354f9f
Kuift Kotar <kuiftk@gmail.com>
2025-04-21 23:36:39 -0400

    update

--- 414/pastest.py
+++
@@ -1,8 +0,0 @@
-import pulser
-
-reg2 = pulser.Register.from_coordinates(
-    [[0, 0], [5, 0], [0, 5], [5, 5]],  # Takes just the coordinates
-    prefix="q",  # All qubit IDs will start with 'q'
-    center=True,
-)
-print("(Centered) qubits:", reg2.qubits)
